6de916fe77b137f426462a6888ab804867758577
Marcos.Xavier <@marcosxavierdev>
2021-10-26 13:57:46 -0300

    first commit

---
+++ src/main/java/dev/edescart/EdescartApiBeApplication.java
@@ -0,0 +1,13 @@
+package dev.edescart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdescartApiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdescartApiBeApplication.class, args);
+	}
+
+}
